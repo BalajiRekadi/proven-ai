4d010487903f9c8760758b49ee939d6dce23d2fc
Balaji Rekadi <64888503+BalajiRekadi@users.noreply.github.com>
2024-07-22 21:35:07 +0530

    Minor fix

--- src/api/helpers/fetchWorksheets.js
+++ src/api/helpers/fetchWorksheets.js
@@ -1,7 +1,6 @@
-//TODO: replace with product param
 const fetchWorksheets = async (product) => {
   const res = await fetch(
-    `https://8df4-2405-201-c03a-a1f1-8493-81f2-6680-8b27.ngrok-free.app/generate_worksheet/?Product=${"3000714"}`,
+    `https://8df4-2405-201-c03a-a1f1-8493-81f2-6680-8b27.ngrok-free.app/generate_worksheet/?Product=${product}`,
     {
       headers: new Headers({
         "ngrok-skip-browser-warning": "69420",
