6a0f2ce6db30a8bb2d64cdbac0407cce8945df0e
Balaji Rekadi <64888503+BalajiRekadi@users.noreply.github.com>
2024-08-01 12:27:33 +0530

    user component revamp

--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -1,8 +1,7 @@
-
-import { AppShell, Flex, Burger } from "@mantine/core"; // Make sure to import Flex
+import { AppShell, Flex, Burger, Image, Text } from "@mantine/core"; // Make sure to import Flex
 import { useDisclosure } from "@mantine/hooks";
 import React from "react";
-import headerLogo from "./../../assets/headerLogo.jpeg";
+import headerLogo from "./../../assets/headerLogo.png";
 import { Outlet } from "react-router-dom";
 import Routes from "../routes/Routes";
 import "./user.css";
@@ -16,26 +15,34 @@ const User = () => {
       className="user"
       header={{ height: 60 }}
       navbar={{
-        width: opened ? 300 : 0,
-        hidden: !opened,
+        width: 300,
+        breakpoint: "sm",
+        collapsed: { desktop: !opened },
       }}
       padding="md"
       h={"100%"}
     >
       <AppShell.Header className="header">
-        <Flex alignSelf="center" px="md" style={{margin:"1%"}}>
-          <div>
-            <Burger opened={opened} onClick={toggle} aria-label="Toggle navigation" color="white" />
-          </div>
-          <img src={headerLogo} className="logo" alt="Header Logo" />
-          <Flex justify="space-between"  style={{ flexGrow: 1, marginLeft: '16px' }}>
-            <ModuleSelect  />
-            <span style={{ color: "white" }}>90001 | Super Admin</span>
+        <Flex>
+          <Flex px="md" w={300} h={60} gap={16} align={"center"}>
+            <Burger
+              opened={opened}
+              onClick={toggle}
+              aria-label="Toggle navigation"
+              color="white"
+            />
+            <Image src={headerLogo} w={"10rem"} h={"2rem"} alt="Header Logo" />
+          </Flex>
+          <Flex justify="space-between" flex={1} align={"center"} h={60}>
+            <ModuleSelect />
+            <Text pr={32} c={"white"}>
+              90001 | Super Admin
+            </Text>
           </Flex>
         </Flex>
       </AppShell.Header>
       {opened && (
-        <AppShell.Navbar p="md" className="navbar" style={{ width: 300, overflow: 'hidden', transition: 'width 0.3s ease' }}>
+        <AppShell.Navbar p="md" className="navbar" w={300}>
           <Routes />
         </AppShell.Navbar>
       )}
